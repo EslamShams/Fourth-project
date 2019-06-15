3347082d724188020a2541ea6b08bf0513705a89
Eslam Shams <eslamshams32@gmail.com>
2019-06-15 21:25:12 +0200

    Finally Finished

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -16,26 +16,29 @@ let mySpans = document.getElementsByTagName('span'),
 
 class Gallery extends Component {
         state={
-                IsToggle : false
+                IsToggle1 : false,
+                IsToggle2 : false,
+                IsToggle3 : false,
+                IsToggle4 : false
+
         }
-IsToggle=()=>{
-        let {IsToggle} = this.state;
-        this.setState({
-                IsToggle : !IsToggle
-        })
-}               
+              
     
 natureComp = ()=>{
-        let {IsToggle} = this.state;
-         if(IsToggle) {
+        let {IsToggle1} = this.state.IsToggle1;
+         if(IsToggle1) {
                 mySpans[5].style.display ="none";
                 mySpans[6].style.display ="none"; 
-                this.IsToggle();
+                this.setState({
+                        IsToggle1 : !IsToggle1
+                })
          }
          else{
                 mySpans[5].style.display ="block";
                 mySpans[6].style.display ="block"; 
-                this.IsToggle();
+                this.setState({
+                        IsToggle1 : !IsToggle1
+                })
          }
               
 
@@ -73,16 +76,20 @@ darkRoad=()=>{
          
 flowerComp = ()=>{
     
-        let {IsToggle} = this.state;
-        if(IsToggle) {
+        let {IsToggle2} = this.state.IsToggle2;
+        if(IsToggle2) {
                mySpans[8].style.display ="none";
                mySpans[9].style.display ="none"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle2 : !IsToggle2
+        })
         }
         else{
                mySpans[8].style.display ="block";
                mySpans[9].style.display ="block"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle2 : !IsToggle2
+        })
         }
             
         }      
@@ -115,16 +122,20 @@ flo2=()=>{
             
 coolComp = () =>{
     
-        let {IsToggle} = this.state;
-        if(IsToggle) {
+        let {IsToggle3} = this.state.IsToggle3;
+        if(IsToggle3) {
                mySpans[11].style.display ="none";
                mySpans[12].style.display ="none"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle3 : !IsToggle3
+        })
         }
         else{
                mySpans[11].style.display ="block";
                mySpans[12].style.display ="block"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle3 : !IsToggle3
+        })
         }
     
         }
@@ -157,16 +168,20 @@ col2=()=>{
                 }         
                        
 cuteComp = () =>{
-        let {IsToggle} = this.state;
-        if(IsToggle) {
+        let {IsToggle4} = this.state.IsToggle4;
+        if(IsToggle4) {
                mySpans[14].style.display ="none";
                mySpans[15].style.display ="none"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle4 : !IsToggle4
+        })
         }
         else{
                mySpans[14].style.display ="block";
                mySpans[15].style.display ="block"; 
-               this.IsToggle();
+               this.setState({
+                IsToggle4 : !IsToggle4
+        })
         }  
     
         }      
@@ -200,30 +215,33 @@ cut2=()=>{
 
 
     render(){
-        let {IsToggle} = this.state;
+        let {IsToggle1} = this.state.IsToggle1;
+        let {IsToggle2} = this.state.IsToggle2;
+        let {IsToggle3} = this.state.IsToggle3;
+        let {IsToggle4} = this.state.IsToggle4;
        
         return(
              <div className="gallery">
                 <div className="component">
-                <span onClick={this.natureComp}>Nature {IsToggle ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}</span>
+                <span onClick={this.natureComp}>Nature {IsToggle1 ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}</span>
                 <span onClick={this.sky}>Sky</span>
                 <span onClick={this.darkRoad}>Dark Road</span>
                 
                
-                <span onClick={this.flowerComp}>Flower {IsToggle ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
+                <span onClick={this.flowerComp}>Flower {IsToggle2 ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
                 
                 <span onClick={this.flo1}>Zoom In Single Flower</span>
                 <span onClick={this.flo2}>Mixed Color Flowers</span>
                 
                 
-                <span onClick={this.coolComp}>Cool {IsToggle ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
+                <span onClick={this.coolComp}>Cool {IsToggle3 ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
                 
                 <span onClick={this.col1}>ColorFull Wall</span>
                 <span onClick={this.col2}>ColorFull Circle</span>
                 
                 
                
-                <span onClick={this.cuteComp}>Cute {IsToggle ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
+                <span onClick={this.cuteComp}>Cute {IsToggle4 ? <i class="fas fa-arrow-circle-up"></i> : <i class="fas fa-arrow-alt-circle-down"></i>}  </span>
                 
                 <span onClick={this.cut1}>Cat</span>
                 <span onClick={this.cut2}>cute cat and dog</span>
